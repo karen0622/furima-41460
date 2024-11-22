8075add2ea09c2b3735d1456d7de8b925a0f0ff4
karen0622 <k.k.nanamei0622@softbank.ne.jp>
2024-11-22 15:39:11 +0900

    バリテーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,20 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  #空の投稿を保存できないようにする
+  validates presence: true do
+    validates: user
+    validates: name
+    validates: explanation
+    validates: price,format: { with: /\A[0-9]+\z/ },inclusion: { in: 300..9_999_999,message: "must be between 300 and 9,999,999"}
+  end
+
+   #ジャンルの選択が「---」の時は保存できないようにする
+   validates numericality: { other_than: 1 } do
+    validates: category_id
+    validates: product_condition_id
+    validates: shipping_cost_id
+    validates: shipping_date_id
+    validates: aria_id
+  end
 end
