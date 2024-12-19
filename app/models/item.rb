35fe4afbfdd99f190167865e633b733cd5a853cb
karen0622 <k.k.nanamei0622@softbank.ne.jp>
2024-12-19 20:06:33 +0900

    関連機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  has_one :purchase
+  has_one_attached :image
 
   belongs_to :category
   belongs_to :product_condition
